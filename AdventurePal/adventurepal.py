bf73078a63ce121d23f04d8daf13ed7777032da4
Duncan Uszkay <Duncan.uszkay@hotmail.com>
2019-02-22 16:03:18 -0500

    just before adding channel as arg

--- AdventurePal/adventurepal.py
+++ AdventurePal/adventurepal.py
@@ -1,5 +1,5 @@
 from redbot.core import commands, checks, Config
-from .story import Story, StoryEncoder
+from .story import Story
 import discord
 import aiohttp
 import json
@@ -8,11 +8,16 @@ CONFIG_IDENTIFIER = 2389284938
 
 ERROR = "An error has occured."
 FAILED_BOOKMARK_RESET = "The bookmark you entered does not match any bookmark you've reached yet."
+BOOKMARKS_LIST_TITLE = "Bookmarks"
+BOOKMARK_FIELD = "Bookmark"
+DESCRIPTION_FIELD = "Description"
+OPTIONS_FIELD = "Options"
 BAD_END_MESSAGE = "You did not choose wisely. This was not the way it was meant to end."
 VICTORY_MESSAGE = "You have found a true ending of the story."
 NO_VALID_STORY = "You must pass your story as an attachment for this to work."
 FAILED_TO_OPEN_FILE = "Failed to read your story file."
 INVALID_OPTION = "{} is not a valid option. Pick one of the numbers shown."
+INVALID_START_STATE = "Loaded story has invalid starting state."
 RESET_MESSAGE = """
 You may choose to return to any bookmark you have already reached.
 Check the list of bookmarks you have already reached by typing:
@@ -25,62 +30,104 @@ Return to one of your choosing by typing:
 class AdventurePal(commands.Cog):
     def __init__(self):
         self.config = Config.get_conf(self, identifier=CONFIG_IDENTIFIER)
-        #sample_story = Story()
-        #sample_story.load_sample()
         default_global = { "channels": [] }
-        default_channel = { "story": None }
+        default_channel = { "story": {} }
         self.config.register_global(**default_global)
         self.config.register_channel(**default_channel)
 
-    def with_story(func):
+    def __with_story(func):
         async def get_and_save_story(self, ctx, *args, **kwargs):
             channel_config = self.config.channel(ctx.channel)
-            story_json = await channel_config.story()
-            story = StoryEncoder.from_json(story_json)
-            story = await func(self, ctx, story, *args, **kwargs)
-            if story:
-                await channel_config.story.set(StoryEncoder.to_json(story))
+            story_hash = await channel_config.story()
+            story = Story()
+            story.load_from_hash(story_hash)
+            story_changes = await func(self, ctx, story, *args, **kwargs)
+            if story_changes:
+                async with channel_config.story() as story:
+                    story.update(story_changes)
         return get_and_save_story
 
-    def zero_index_input(func):
-        async def reduce_input(self, ctx, story, option, *args, **kwargs):
-            if option.isdigit():
-                option = str(int(option) - 1)
-            return await func(self, ctx, story, option, *args, **kwargs)
-        return reduce_input
+    def __collapse_args(func):
+        async def collapsed_args(self, ctx, story, *args):
+            return await func(self, ctx, story, ' '.join(args))
+        return collapsed_args
+
+    @commands.group()
+    async def advpal(self, ctx: commands.Context):
+        pass
+
+    @advpal.command(name="load")
+    @__with_story
+    async def load(self, ctx, story):
+        print(ctx.clean_prefix)
+        if len(ctx.message.attachments) == 0:
+            await self.__error_message(ctx, NO_VALID_STORY)
+            return
+        attachment_hash = await self.load_attachment(ctx.message)
+        if attachment_hash and story.load_from_hash(attachment_hash):
+            initial_state = story.set_initial_state()
+            if initial_state == {}:
+                await self.__error_message(ctx, INVALID_START_STATE)
+                return
+            attachment_hash.update(initial_state)
+            await self.communicate_state(ctx, story)
+            return attachment_hash
+        await self.__error_message(ctx, FAILED_TO_OPEN_FILE)
+
+    @advpal.command(name="choose")
+    @__with_story
+    @__collapse_args
+    async def choose(self, ctx, story, option):
+        changes = story.choose_option(option)
+        if changes:
+            await self.communicate_state(ctx, story)
+            return changes
+        await self.__error_message(ctx, INVALID_OPTION.format(option))
+
+    @advpal.command(name = "reset")
+    @__with_story
+    @__collapse_args
+    async def reset(self, ctx, story, bookmark):
+        changes = story.bookmark_reset(bookmark)
+        if changes:
+            await self.communicate_state(ctx, story)
+            return changes
+        await self.__error_message(ctx, FAILED_BOOKMARK_RESET)
+
+    @advpal.command(name="bookmarks")
+    @__with_story
+    async def bookmarks(self, ctx, story):
+        em = discord.Embed(title=BOOKMARKS_LIST_TITLE, description=story.bookmarks_string())
+        await ctx.send(embed=em)
 
     def build_state_embed(self, story):
-        state_data = story.get_state_data()
-        options = story.options_string()
-        imgsrc = state_data.get("imgsrc", None)
         em = discord.Embed()
-        em.add_field(name="Bookmark", value=state_data["bookmark"])
-        em.add_field(name="Description", value=state_data["text"])
-        if options:
-            em.add_field(name="Options", value=options)
-        if imgsrc:
-            em.set_image(url=imgsrc)
+        em.add_field(name=BOOKMARK_FIELD, value=story.bookmark())
+        em.add_field(name=DESCRIPTION_FIELD, value=story.text())
+        options = story.options()
+        if options: em.add_field(name=OPTIONS_FIELD, value=options)
+        imgsrc = story.image()
+        if imgsrc: em.set_image(url=imgsrc)
         return em
 
-    async def load_attachment(self, url):
+    async def load_attachment(self, message):
+        story_attachment = message.attachments[0]
         async with aiohttp.ClientSession() as session:
-            async with session.get(url) as r:
-                if r.status != 200:
-                    return False
-                try:
-                  contents = await r.json()
+            async with session.get(story_attachment.url) as r:
+                if r.status != 200: return False
+                try: contents = await r.json()
                 except (aiohttp.client_exceptions.ContentTypeError, json.decoder.JSONDecodeError):
                   return False
                 return contents
 
     async def communicate_state(self, ctx, story):
         await ctx.send(embed=self.build_state_embed(story))
-        await self.maybe_end_message(ctx, story)
+        await self.__maybe_end_message(ctx, story)
 
-    async def error_message(self, ctx, error):
+    async def __error_message(self, ctx, error):
         await ctx.send(embed=discord.Embed(title=ERROR, description=error))
 
-    async def maybe_end_message(self, ctx, story):
+    async def __maybe_end_message(self, ctx, story):
         ending = story.get_ending()
         message = None
         if ending:
@@ -90,73 +137,3 @@ class AdventurePal(commands.Cog):
             em = discord.Embed(description=RESET_MESSAGE, title=BAD_END_MESSAGE)
           await ctx.send(embed=em)
 
-    async def on_message(self, message):
-        print("on message")
-        channel = message.channel
-        channels = await self.config.channels()
-        if message.channel in channels and not message.bot:
-            self.choose(self, message, message.text)
-
-
-    @commands.group()
-    async def advpal(self, ctx: commands.Context):
-        pass
-
-    @advpal.command(name="choose")
-    @with_story
-    @zero_index_input
-    async def choose(self, ctx, story, option):
-        print("time to choose")
-        if not option.isdigit() or story.choose_option(int(option)) is None:
-            await self.error_message(ctx, INVALID_OPTION.format(option))
-            return
-        await self.communicate_state(ctx, story)
-        return story
-
-
-    @advpal.command(name="reset_sample")
-    @with_story
-    async def reset_sample(self, ctx, story):
-        story.load_sample()
-        story.set_initial_state()
-        return story
-
-    @advpal.command(name="load")
-    @with_story
-    async def load(self, ctx, story):
-        if len(ctx.message.attachments) == 0:
-            await self.error_message(ctx, NO_VALID_STORY)
-            return
-        story_attachment = ctx.message.attachments[0]
-        attachment_json = await self.load_attachment(story_attachment.url)
-        if attachment_json and story.load_from_json(attachment_json):
-            await self.communicate_state(ctx, story)
-            return story
-        await self.error_message(ctx, FAILED_TO_OPEN_FILE)
-
-    @advpal.command(name="start")
-    @with_story
-    async def start(self, ctx, story):
-        story.set_initial_state()
-        await self.communicate_state(ctx, story)
-        return story
-
-    @advpal.group()
-    async def bookmarks(self, ctx: commands.Context):
-        pass
-
-    @bookmarks.command(name = "list")
-    @with_story
-    async def list(self, ctx, story):
-        em = discord.Embed(title="Bookmarks", description=story.bookmarks_string())
-        await ctx.send(embed=em)
-
-    @bookmarks.command(name = "reset")
-    @with_story
-    @zero_index_input
-    async def reset(self, ctx, story, bookmark):
-        if bookmark.isdigit() and story.bookmark_reset(int(bookmark)):
-            await self.communicate_state(ctx, story)
-            return story
-        await self.error_message(ctx, FAILED_BOOKMARK_RESET)
-        await self.list(ctx, story)
--- AdventurePal/story.py
+++ AdventurePal/story.py
@@ -1,130 +1,85 @@
+from enum import Enum
 import json
-sample_states = [
-  {
-    "bookmark": "Dark Room",
-    "text": "You are in a dark room. To your left is a door, to your right is a hole. Do you go left or right?",
-    "imgsrc": "https://www.escapeall.gr/Images/ServicesPhotos/d0d9794b-573a-4a56-80e1-7015acc2ae7f/e1585535-c825-48c6-b5ea-6f181155e528.jpg",
-    "options": [
-        {
-            "text": "go left",
-            "state": 1
-        },
-        {
-            "text": "go right",
-            "state": 2
-        }
-    ]
-  },
-  {
-    "bookmark": "Sweet Victory",
-    "text": "Behind the doors lies an enourmous stash of sealed blue ray copies of madagascar. Congradulations",
-    "imgsrc": "https://images-na.ssl-images-amazon.com/images/I/91EPpEmV-qL._SX425_.jpg",
-    "options": [],
-    "ending": "good"
-  },
-  {
-    "bookmark": "Endless Shaft",
-    "text": "You are now tumbling down an endless shaft into the bowels of the earth. You may now do a flip if you wish.",
-    "imgsrc": "https://i.ytimg.com/vi/E4flwk5rp8I/hqdefault.jpg",
-    "options": [
-        {
-            "text": "do a flip",
-            "state": 3
-        }
-    ]
-  },
-  {
-    "bookmark": "Death",
-    "text": "The hole goes down for hundreds of miles. You die instantly on impact with the ground. RIP",
-    "imgsrc": "https://new-img.patrika.com/upload/2017/08/28/demo_pic_-_patrika_chhattisgarh__1756429_835x547-m.jpeg",
-    "options": [],
-    "ending": "bad"
-  }
-]
-
 START_STATE = 0
 
-class StoryEncoder:
-    @staticmethod
-    def to_json(story):
-        return{
-            "states": story.states,
-            "title": story.title,
-            "author": story.author,
-            "state": story.state,
-            "bookmarks": story.bookmarks
-        }
-
-    @staticmethod
-    def from_json(json):
-        story = Story()
-        story.states = json["states"]
-        story.title = json["title"]
-        story.author = json["author"]
-        story.state = json["state"]
-        story.bookmarks = json["bookmarks"]
-        return story
-
+class MODIFICATION_LEVEL(Enum):
+    CLEAN = 0
+    STATE_ONLY = 1
+    FULL = 2
 
 class Story:
-    def __init__(self):
-        self.states = []
-        self.title = "NO STORY LOADED"
-        self.author = ""
-        self.set_initial_state()
-
-    def load_sample(self):
-        self.states = sample_states
-        self.title = "Sample Story"
-        self.author = "Kilgore Trout"
-
-    def load_from_json(self, json):
-        try:
-            self.states = json["states"]
-            self.title = json["title"]
-            self.author = json["author"]
-        except KeyError:
-            return False
-        self.set_initial_state()
-        return True
-
     def set_initial_state(self):
-        self.state = START_STATE
-        self.bookmarks = [START_STATE]
+        self.bookmarks = []
+        return self.__maybe_transfer_state(self.start)
 
-    def get_state_data(self):
-        return self.states[self.state]
-
-    def numbered_list(self, array, represent_func):
-        return "\n".join([str(i+1) + ": " + represent_func(a) for i,a in enumerate(array)])
-
-    def options_string(self):
-        options = self.get_state_data()["options"]
-        if options:
-            return self.numbered_list(options, lambda o: o["text"])
-        return None
+    def load_from_hash(self, story_hash):
+        try:
+            self.states = story_hash["states"]
+            self.title = story_hash["title"]
+            self.author = story_hash["author"]
+            self.start = story_hash["start"]
+        except KeyError:
+            return {}
+        self.state = story_hash.get("state", None)
+        self.bookmarks = story_hash.get("bookmarks", [])
+        self.modification_level = MODIFICATION_LEVEL.CLEAN
+        return self.__hash()
 
     def bookmarks_string(self):
-        return self.numbered_list(self.bookmarks, lambda b: self.states[b]["bookmark"])
+        return "\n".join(self.bookmarks)
 
     def get_ending(self):
-        return self.get_state_data().get("ending", None)
+        return self.__get_state_data().get("ending", None)
 
-    def transfer_state(self, state):
-        self.state = state
-        self.bookmarks.append(state)
+    def choose_option(self, option):
+        next_state = self.__get_state_data()["options"].get(option, None)
+        return self.__maybe_transfer_state(next_state)
 
-    def choose_option(self, index):
-        options = self.get_state_data()["options"]
-        if index < 0 or index >= len(options):
-            return False
-        self.transfer_state(options[index]["state"])
-        return True
+    def bookmark_reset(self, bookmark):
+        bookmark = (bookmark if bookmark in self.bookmarks else None)
+        return self.__maybe_transfer_state(bookmark)
 
-    def bookmark_reset(self, index):
-        if index < 0 or index >= len(self.bookmarks):
-            return False
-        self.state = self.bookmarks[index]
-        return True
+    def bookmark(self):
+        return self.__get_state_data()["bookmark"]
 
+    def text(self):
+        return self.__get_state_data()["text"]
 
+    def image(self):
+        return self.__get_state_data().get("imgsrc", None)
+
+    def options(self):
+        options = self.__get_state_data()["options"]
+        if options:
+            return "\n".join(options.keys())
+        return None
+
+    def __hash(self):
+        story_hash = {
+            "states": self.states,
+            "title": self.title,
+            "author": self.author,
+            "start": self.start
+        }
+        story_hash.update(self.__state_hash())
+        return story_hash
+
+    def __state_hash(self):
+        return {
+            "state": self.state,
+            "bookmarks": list(self.bookmarks),
+        }
+
+    def __maybe_transfer_state(self, state):
+        if state:
+            self.state = state
+            self.__add_bookmark(state)
+            return self.__state_hash()
+        return {}
+
+    def __add_bookmark(self, bookmark):
+        if not bookmark in self.bookmarks:
+            self.bookmarks.append(bookmark)
+
+    def __get_state_data(self):
+        return self.states[self.state]
